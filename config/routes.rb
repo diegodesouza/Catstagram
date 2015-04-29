af6ba5f8c4dfab1a8ca3a323d91dc9b22861dd08
Diego de Souza <diegotrus7@gmail.com>
2015-04-29 15:13:59 -0400

    Created first test - User Sings Up Added Devise, g views and user Created route for pages, pages controller, and pages index All tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  # Set the index action of a PagesController as the root route
+  root 'pages#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
